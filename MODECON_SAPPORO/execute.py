04a65587c6012d6d71e24883dab64b8a93778aa1
paisen4312 <syvlebgth@gmail.com>
2021-12-15 23:32:51 +0900

    RT数取得パッケージの追加

---
+++ MODECON_SAPPORO/execute.py
@@ -0,0 +1,11 @@
+# coding: utf-8
+import ope
+
+#1回目だけ
+#
+#ope.makeSheets()
+ope.hasListUser()
+#ope.notHasListUser()
+#ope.getLivePoint()
+#ope.csvToExcel()
+#ope.livePointToExcel()
---
+++ MODECON_SAPPORO/participants.py
@@ -0,0 +1,26 @@
+dict = {}
+users_dict = {}
+all_dict = {}
+elems_dict = {}
+
+#リストを持っているユーザーのdictを作成
+def makeListUsers():
+
+    dict["Misaki_n_1127"] = "水咲"
+    pass
+
+#リストがない人たちのdictを作成
+def makeUsers():
+    users_dict[""] = ""
+    pass
+
+#全員分のdictを作成
+def makeAllUsers():
+    all_dict["Misaki_n_1127"] = "水咲"
+    pass
+
+def makeElemsDict():
+    elems_dict[""] = "水咲"
+
+
+    pass
