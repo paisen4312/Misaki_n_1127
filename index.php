859277a3237734b30fc93e41f808323fde74abf3
paisen4312 <61623989+paisen4312@users.noreply.github.com>
2021-12-15 21:53:14 +0900

    Create index.php

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=0.3">
+    <title>集計ページ</title>
+    <link rel="stylesheet" href="css/common.css"/>
+</head>
+<body>    
+    <main>
+        <div style = "margin-top: 50px;text-align: center;">
+            <button style = "background: #dfdfdf; text-align: center; height : 50px; width : 300px; font-size: 20px;" name = "button">
+                集計
+            </button>
+
+        </div>
+
+    </main>
+    <script src="js/lib/jquery-3.4.1.min.js"></script>
+    <script src="js/lib/jquery-ui-1.10.3.custom.min.js"></script>  
+</body>
+</html>
